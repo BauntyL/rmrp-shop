9ee09cba948bc4ea55f6539948bc94c07267b3c0
BauntyL <bauntyxr@mail.ru>
2025-05-31 14:41:35 +0200

    Refactor: Clean project structure and fix build issues

--- server.js
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-// Импортируем скомпилированное приложение
-import('./server/dist/index.js').catch(error => {
-  console.error('Ошибка при запуске сервера:', error);
-  process.exit(1);
-});

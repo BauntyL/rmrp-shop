6fbbefa9bf1f25f58170fd673b592b8a2d39a2ee
BauntyL <bauntyxr@mail.ru>
2025-06-02 03:09:08 +0200

    fix: correct export patterns in steps index file

--- client/src/components/steps/index.ts
+++ client/src/components/steps/index.ts
@@ -9,13 +9,13 @@ export { default as TreasureStep2 } from './treasure-step2';
 export { default as TreasureStep3 } from './treasure-step3';
 
 // Шаги для автомобилей
-export { default as CarStep1 } from './car-step1';
-export { default as CarStep2 } from './car-step2';
-export { default as CarStep3 } from './car-step3';
-export { default as CarStep4 } from './car-step4';
+export { CarStep1 } from './car-step1';
+export { CarStep2 } from './car-step2';
+export { CarStep3 } from './car-step3';
+export { CarStep4 } from './car-step4';
 
 // Шаги для недвижимости
-export { default as RealEstateStep1 } from './realestate-step1';
-export { default as RealEstateStep2 } from './realestate-step2';
-export { default as RealEstateStep3 } from './realestate-step3';
-export { default as RealEstateStep4 } from './realestate-step4';
+export { RealEstateStep1 } from './realestate-step1';
+export { RealEstateStep2 } from './realestate-step2';
+export { RealEstateStep3 } from './realestate-step3';
+export { RealEstateStep4 } from './realestate-step4';

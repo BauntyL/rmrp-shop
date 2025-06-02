ac1d8b42e993989ab60f9fea3f9c50d0d46d751c
BauntyL <bauntyxr@mail.ru>
2025-06-02 06:38:50 +0200

    Restructure real estate modal: simplify from 4 to 3 steps
    - Create new RealEstateStep1: property type, server, title, price
    - Create new RealEstateStep2: garage spaces, warehouses, helipads, income, description
    - Create new RealEstateStep3: image URL and contacts (Discord, Telegram, phone)
    - Update CreateRealEstateModal to use new 3-step structure
    - Remove redundant fields and improve user experience
    - Add proper validation and modern design with icons

--- client/src/components/steps/index.ts
+++ client/src/components/steps/index.ts
@@ -6,8 +6,6 @@ export { default as TreasureStep2 } from './treasure-step2';
 export { default as CarStep1 } from './car-step1';
 export { default as CarStep2 } from './car-step2';
 export { default as CarStep3 } from './car-step3';
-export { default as GeneralStep1 } from './general-step1';
-export { default as GeneralStep2 } from './general-step2';
 export { RealEstateStep1 } from './realestate-step1';
 export { RealEstateStep2 } from './realestate-step2';
 export { RealEstateStep3 } from './realestate-step3';
